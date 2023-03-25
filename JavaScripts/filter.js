271873eeed512f17a63960802fbc16de7742e00b
DIVYA RAJESWARI PAMULA <divyarajeswaripamula@gmail.com>
2023-03-25 00:01:52 +0000

    FILTER BY CATEGORY AND PRICE

--- JavaScripts/filter.js
+++ JavaScripts/filter.js
@@ -1,165 +1,77 @@
-function filter(){
-    let s=document.querySelector('#shop');
-    s.innerHTML="";
-    
+const endpoint = 'http://localhost:3000/products';
+
+const filtersContainer = document.getElementById('filters-container');
+const categorySelect = document.getElementById('category');
+const priceSelect = document.getElementById('price');
+const productsContainer = document.getElementById('shop');
+
+// Fetch the products from the API and display them on the page
+fetch(endpoint)
+  .then(response => response.json())
+  .then(products => {
+    displayProducts(products);
+
+    categorySelect.addEventListener('change', () => {
+      const filteredProducts = filterProducts(products, categorySelect.value, priceSelect.value);
+      displayProducts(filteredProducts);
+    });
+
+    priceSelect.addEventListener('change', () => {
+      const filteredProducts = filterProducts(products, categorySelect.value, priceSelect.value);
+      displayProducts(filteredProducts);
+    });
+  })
+  .catch(error => console.error(error));
+
+// Create and display the product cards on the page
+function displayProducts(products) {
+  productsContainer.innerHTML = '';
+  products.forEach(product => {
+      const li = document.createElement('div');
+
+     
+        li.innerHTML = `
+        <div id=product-id-${product.id} class="item">
+        <img width="220" src=${product.img} alt="">
+        <div class="details">
+          <h3>${product.name}</h3>
+          <p>${product.desc}</p>
+
+          <div class="price-quantity">
+            <h2>$ ${product.price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${product.id})" class="bi bi-dash-lg"></i>
+              <div id=${product.id} class="quantity"></div>
+              <i onclick="increment(${product.id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+        `;
+        productsContainer.appendChild(li);
+      
+    });
   
-    for(i=0;i<data.length;i++){
-      var data_filter =data[i];
-      console.log(data_filter);
-      if(data_filter.name=="Shampoo Bar"||data_filter.name=="shampoo Bar" ){
-        const elem = document.createElement("div");
-        elem.innerHTML = `<div id=product-id-${data_filter.id} class="item">
-        <img width="220" src=${data_filter.img} alt="">
-        <div class="details">
-          <h3>${data_filter.name}</h3>
-          <p>${data_filter.desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${data_filter.price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${data_filter.id})" class="bi bi-dash-lg"></i>
-              <div id=${data_filter.id} class="quantity">
-            
-        </div>
-              <i onclick="increment(${data_filter.id})" class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-    </div>`
-        console.log(elem);
-        s.appendChild(elem)
+}
+
+// Filter the products array by category and price range
+function filterProducts(products, category, priceRange) {
+  return products.filter(product => {
+    // Filter by category
+    if (category && product.category !== category) {
+      return false;
+    }
+
+    // Filter by price range
+    if (priceRange) {
+      const price = parseInt(product.price);
+      const [minPrice, maxPrice] = priceRange.split('-').map(range => parseInt(range));
+
+      if (price < minPrice || price > maxPrice) {
+        return false;
       }
     }
-   
-  }
-  function filter1(){
-    let s=document.querySelector('#shop');
-    s.innerHTML="";
-    for(i=0;i<data.length;i++){
-      var data_filter =data[i];
-      console.log(data_filter);
-      if(data_filter.name=="Moisturizer"||data_filter.name=="moisturizer" ){
-        const elem = document.createElement("div");
-        elem.innerHTML = `<div id=product-id-${data_filter.id} class="item">
-        <img width="220" src=${data_filter.img} alt="">
-        <div class="details">
-          <h3>${data_filter.name}</h3>
-          <p>${data_filter.desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${data_filter.price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${data_filter.id})" class="bi bi-dash-lg"></i>
-              <div id=${data_filter.id} class="quantity">
-            
-        </div>
-              <i onclick="increment(${data_filter.id})" class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-    </div>`
-        console.log(elem);
-        s.appendChild(elem)
-      }else{
-        console.log("no data");
-      }
-    }
-   
-  }
-  function filter2(){
-    let s=document.querySelector('#shop');
-    s.innerHTML="";
-    for(i=0;i<data.length;i++){
-      var data_filter =data[i];
-      console.log(data_filter);
-      if(data_filter.name=="Moisturizing Cream" ){
-        const elem = document.createElement("div");
-        elem.innerHTML = `<div id=product-id-${data_filter.id} class="item">
-        <img width="220" src=${data_filter.img} alt="">
-        <div class="details">
-          <h3>${data_filter.name}</h3>
-          <p>${data_filter.desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${data_filter.price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${data_filter.id})" class="bi bi-dash-lg"></i>
-              <div id=${data_filter.id} class="quantity">
-            
-        </div>
-              <i onclick="increment(${data_filter.id})" class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-    </div>`
-        console.log(elem);
-        s.appendChild(elem)
-      }else{
-        console.log("no data");
-      }
-    }
-   
-  }
-  function filter3(){
-    let s=document.querySelector('#shop');
-    s.innerHTML="";
-    for(i=0;i<data.length;i++){
-      var data_filter =data[i];
-      console.log(data_filter);
-      if(data_filter.name=="Serum"){
-        const elem = document.createElement("div");
-        elem.innerHTML = `<div id=product-id-${data_filter.id} class="item">
-        <img width="220" src=${data_filter.img} alt="">
-        <div class="details">
-          <h3>${data_filter.name}</h3>
-          <p>${data_filter.desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${data_filter.price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${data_filter.id})" class="bi bi-dash-lg"></i>
-              <div id=${data_filter.id} class="quantity">
-            
-        </div>
-              <i onclick="increment(${data_filter.id})" class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-    </div>`
-        console.log(elem);
-        s.appendChild(elem)
-      }else{
-        console.log("no data");
-      }
-    }
-   
-  }
-  function filter4(){
-    let s=document.querySelector('#shop');
-    s.innerHTML="";
-    for(i=0;i<data.length;i++){
-      var data_filter =data[i];
-      console.log(data_filter);
-      if(data_filter.name=="Oil"){
-        const elem = document.createElement("div");
-        elem.innerHTML = `<div id=product-id-${data_filter.id} class="item">
-        <img width="220" src=${data_filter.img} alt="">
-        <div class="details">
-          <h3>${data_filter.name}</h3>
-          <p>${data_filter.desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${data_filter.price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${data_filter.id})" class="bi bi-dash-lg"></i>
-              <div id=${data_filter.id} class="quantity">
-            
-        </div>
-              <i onclick="increment(${data_filter.id})" class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-    </div>`
-        console.log(elem);
-        s.appendChild(elem)
-      }else{
-        console.log("no data");
-      }
-    }
-   
-  }
+
+    return true;
+  });
+}
