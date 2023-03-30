74bb4556faadddc6cb5f384a965d4a4f1d89e359
SurajKumarMR <116290430+SurajKumarMR@users.noreply.github.com>
2023-03-30 14:06:57 +0100

    Add files via upload
    Admin Page

---
+++ Java/AdminPage/src/main/java/site/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.israorganic.site.repository;
+
+import com.israorganic.site.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ Java/AdminPage/src/main/java/site/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.israorganic.site.service.dto;
